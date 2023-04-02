d35d93ed61d2024c94c2db5307d52143dce88423
Jan Molic <jan.molic@erasys.de>
2023-04-02 20:37:55 +0200

    fix

--- robot.js
+++ robot.js
@@ -121,6 +121,7 @@ function robot(question) {
   question = ' ' + question + ' '
   let answer = matchQuestion(question, definition);
   if (answer) {
+    alert(question)
     if (question!='eee')
       answer = prefixAnswer(origQuestion, answer)
     answer = replaceSubject(question, answer);
