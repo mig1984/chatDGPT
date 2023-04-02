2e154b721f4857d5fa77c2f3951241680671c1f3
Jan Molic <jan.molic@erasys.de>
2023-04-02 23:56:21 +0200

    fix

--- shared.js
+++ shared.js
@@ -24,7 +24,8 @@ function toggleMode() {
 
 function mainThings(x) {
    x.qsa('a').withText('ChatGPT Mar 14 Version', txt=> {
-      txt.textContent = 'ChatDGPT Nightly Nightmare Version'
+      //txt.textContent = 'ChatDGPT Nightly Nightmare Version'
+      console.log('HERE')
       console.log(txt.parentNode)
       txt.parentNode.src = 'https://github.com/mig1984/chatDGPT'
    })
